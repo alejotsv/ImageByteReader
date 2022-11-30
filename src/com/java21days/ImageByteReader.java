a993a768427ec8f07603f367e30625777e0bc3c4
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 13:56:19 -0500

    Complete class

--- src/com/java21days/ImageByteReader.java
+++ src/com/java21days/ImageByteReader.java
@@ -1,14 +1,25 @@
 package com.java21days;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ImageByteReader {
 
     public ImageByteReader() throws IOException {
-        FileInputStream fileInputStream = new FileInputStream("pagedataicon.gif");
-        System.out.println(fileInputStream.read());
-
+        String path = getClass().getResource("pagedataicon.gif").getPath();
+        System.out.println(path);
+        FileInputStream fileInputStream = new FileInputStream(path);
+        int newByte = 0;
+        while(newByte != -1){
+            newByte = fileInputStream.read();
+            if(newByte == -1){
+                System.out.println("Done");
+                break;
+            }
+            System.out.println(newByte);
+        }
     }
 }
 
--- src/com/java21days/Main.java
+++ src/com/java21days/Main.java
@@ -1,4 +1,9 @@
 package com.java21days;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        new ImageByteReader();
+    }
 }
