f0c91ec226811bb66c555947a223260651035e58
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 16:43:09 -0500

    Generate getters

--- src/com/java21days/ImageByteReader.java
+++ src/com/java21days/ImageByteReader.java
@@ -1,18 +1,17 @@
 package com.java21days;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ImageByteReader {
     byte[] imageBytes;
+    int length;
 
     public ImageByteReader() {
         try {
             String path = getClass().getResource("rm.png").getPath();
             FileInputStream fileInputStream = new FileInputStream(path);
-            int length = fileInputStream.available();
+            length = fileInputStream.available();
             imageBytes = new byte[length];
             fileInputStream.read(imageBytes, 0, length);
 
@@ -22,9 +21,18 @@ public class ImageByteReader {
 
         } catch (IOException e){
             System.out.println(e.getMessage());
+
         } catch (NullPointerException e){
             System.out.println("Path is: " + e.getMessage());
         }
+    }
+
+    public byte[] getImageBytes() {
+        return imageBytes;
+    }
+
+    public int getLength() {
+        return length;
     }
 }
 
