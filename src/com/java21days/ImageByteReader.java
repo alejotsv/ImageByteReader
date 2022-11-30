c11cf5b552c8b622a02bf9c23bc5d28d90c6eba6
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 11:33:26 -0500

    Add main class

--- src/com/java21days/ImageByteReader.java
+++ src/com/java21days/ImageByteReader.java
@@ -1,4 +1,14 @@
 package com.java21days;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ImageByteReader {
+
+    public ImageByteReader() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("pagedataicon.gif");
+        System.out.println(fileInputStream.read());
+
+    }
 }
+
